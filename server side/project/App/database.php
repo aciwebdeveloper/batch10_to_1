46cba3a76cecfc9209512e09591da7c5ce530d07
YSK <yousafsadiq445@gmail.com>
2023-11-30 12:15:45 +0500

    30-nov-2023

--- server side/project/App/database.php
+++ server side/project/App/database.php
@@ -1,7 +1,7 @@
 <?php 
 
 namespace App\Database;
-
+ 
 class database{
  private $server="localhost";
  private $user_name="root";
@@ -18,6 +18,8 @@ private $result;
 
 private $exe;
  
+private $fetchResult=array();
+
 public function __construct(){
 
   $this->link = new \mysqli($this->server,$this->user_name,$this->pass,$this->db);
@@ -120,7 +122,16 @@ $this->Query.= " ORDER BY {$table}.{$primaryKEy} DESC"; // order BY users.user_i
 
 
 if ($limit != null) {
-  $this->Query.= " LIMIT {$limit}";  // LIMIT 4 7
+  if (isset($_GET["page"]) && !empty($_GET["page"])) {
+    $page=$_GET["page"];
+  }else{
+    $page=1;
+  }
+  
+
+  $per_page_record = ($page-1) * $limit;  
+
+ echo $this->Query.= " LIMIT {$per_page_record},{$limit}";  // LIMIT 0 5
 }
 
 
@@ -128,10 +139,113 @@ return $this->execute($this->Query,null);
 
 }
 
-public function GetResult(){
- $this->result= $this->exe->fetch_assoc();
+
+public function pagination($table,$join=null,$where=null,$limit=null){
+$help= new helper();
+
+  if ($limit != null) {
  
- return  $this->result;
+    $this->Query = "select * from `{$table}` ";
+
+    if ($join != null) {
+      $this->Query.="JOIN  {$join} ";
+    }
+  
+    if ($where != null) {
+      $this->Query.="WHERE  {$where} ";
+    }
+
+    $exe=$this->link->query($this->Query);
+
+    $total_records= $exe->num_rows; // total number of records exist in table 
+
+
+    $total_pages= ceil($total_records/$limit); // to findout number of pages 
+
+     $fileURl=basename($_SERVER["PHP_SELF"]); // current page name 
+
+     $page="";
+if (isset($_GET["page"]) && !empty($_GET["page"])) {
+  $page=$_GET["page"];
+}else{
+  $page=1;
+}
+
+
+
+$paginate="<nav aria-label='Page navigation example'>
+<ul class='pagination'>
+  <li class='page-item'>
+    <a class='page-link' href='#' aria-label='Previous'>
+      <span aria-hidden='true'>&laquo;</span>
+    </a>
+  </li>";
+  // http://localhost/batch_10_to_1/server%20side/project/abc.php?page=1
+  // http://localhost/batch_10_to_1/server%20side/project/abc.php?page=3
+  $class="";
+  for ($i=1; $i <= $total_pages ; $i++) { 
+
+  if ($i == $page) {
+  $class ="active";
+}
+else{
+  $class ="";
+}
+
+  $paginate.= "<li class='page-item {$class}'><a class='page-link' href='{$fileURl}?page={$i}'>{$i}</a></li>";
+}
+
+
+
+
+
+  $paginate .= "<li class='page-item'>
+  <a class='page-link' href='#' aria-label='Next'>
+    <span aria-hidden='true'>&raquo;</span>
+  </a>
+</li>
+</ul>
+</nav>";
+
+
+echo $paginate;
+
+  
+  }
+  else{
+    echo $help->ErrorMSG("LIMIT paramter should not be null");
+  }
+
+
+  
+  
+  // $result = mysqli_query($conn, $query);  
+  // $number_of_result = mysqli_num_rows($result);  
+    
+  // //determine the total number of pages available  
+  // $number_of_page = ceil ($number_of_result / $results_per_page);  
+
+
+
+}
+
+
+
+public function GetResult(){
+  $help= new helper;
+//  $this->result= $this->exe->fetch_assoc();
+
+ while ($row=$this->exe->fetch_assoc()) {
+
+
+  array_push($this->fetchResult,$row);
+ 
+}
+
+//  $help->pre($this->fetchResult);
+
+ return  $this->fetchResult;
+
 }
 
 private function GetPrimaryKey($table){
---
+++ server side/project/abc.php
@@ -0,0 +1,114 @@
+<?php
+
+require_once (dirname(__FILE__)."/include/header.php");
+
+use App\Database\database as DB;
+use App\Database\helper as help;
+
+$help=new help();
+$obj= new DB();
+
+// for ($i=1; $i < 50 ; $i++) { 
+ // $abc=[
+ //  "user_name"=> "XYZ2_".rand(1,999),
+ //  "email"=>"XYZ".rand(1,999)."@email.com",
+ //  "password"=>123432
+ // ];
+ 
+ // $obj->MyInsert("users",$abc); 
+ 
+ # code...
+// }
+$abc=[
+ "user_name"=> "XYZ2",
+ "email"=>"XYZ2@email.com",
+ "password"=>123432
+];
+
+// $obj->MyInsert("users",$abc) 
+ 
+try {
+
+$count=1;
+
+$limit=5;
+
+if (isset($_GET["page"]) && !empty($_GET["page"])) {
+ $page=$_GET["page"];
+}else{
+ $page=1;
+}
+
+
+$offset = ($page-1) * $limit;  
+
+$count=($offset+1);
+ // $obj->select("users","users.*,user_task.*","user_task.user_id=6","user_task ON users.user_id =user_task.user_id",null,"10");
+ $obj->select("users","*",null,null,null,$limit);
+
+ $abc=$obj->GetResult();
+ // $help->pre($abc);
+ 
+ ?>
+
+
+<div class="table-responsive">
+
+
+<table class="table table-dark table-hover">
+
+<tr>
+ <th>#</th>
+ <th>userName</th>
+ <th>Email</th>
+</tr>
+
+
+<?php 
+
+
+
+for($j=0; $j < count($abc); $j++ ) {
+ // $help->pre($row);
+ 
+?>
+
+<tr>
+ <td><?php echo $count; ?></td>
+ <td><?php echo $abc[$j]["user_name"]; ?></td>
+ <td><?php echo $abc[$j]["email"]; ?></td>
+</tr>
+
+
+<?php 
+$count++;
+}
+
+?>
+
+
+
+</table>
+
+</div>
+
+
+
+
+<?php
+
+$obj->pagination("users",null,null,$limit);
+
+} catch (\Throwable $th) {
+ throw $th;
+}
+
+?>
+
+
+
+<?php
+
+require_once (dirname(__FILE__)."/include/footer.php");
+
+?>
